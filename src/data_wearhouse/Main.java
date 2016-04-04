dd5cc5802c70db8e8353749650f6336296661b1d
zurekn <zurek.nicolas@gmail.com>
2016-04-04 13:39:20 +0200

    Create the connexion

--- src/data_wearhouse/Main.java
+++ src/data_wearhouse/Main.java
@@ -1,9 +1,12 @@
 package data_wearhouse;
 
+import database.OracleConnexion;
+
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("Hello is the data wearhouse project!");
+		System.out.println("Hello is the data wearhouse project!");
+		OracleConnexion oc = new OracleConnexion();
 	}
 
 }
